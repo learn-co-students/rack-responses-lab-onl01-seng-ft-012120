a2eeea1034cd25ea587753e3e68a7e7e22081d68
oliverfeher <oliver92.feher@gmail.com>
2020-02-21 00:17:52 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,9 @@
+class Application 
+    def call(env)
+        resp = Rack::Response.new
+        t = Time.now
+        (t.hour > 12) ? (resp.write "Good Afternoon!") : (resp.write "Good Morning!")
+        resp.finish
+    end
+
+end
